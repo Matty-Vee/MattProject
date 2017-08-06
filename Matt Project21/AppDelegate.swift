b4f7320a240c93da12c91b3820d3617e1a93b846
Matthew Vaughan <mr.matthewvaughan@gmail.com>
2017-08-06 21:35:07 +1000

    Initial Commit

---
+++ Matt Project21/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Matt Project21
+//
+//  Created by Matthew Vaughan on 6/8/17.
+//  Copyright © 2017 Matthew Vaughan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Matt Project21/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Matt Project21
+//
+//  Created by Matthew Vaughan on 6/8/17.
+//  Copyright © 2017 Matthew Vaughan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
